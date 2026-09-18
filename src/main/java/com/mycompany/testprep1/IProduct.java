20aa9e304daec99c50fb28a4c40e5c0c95ce0d5e
simon-is-a-dev <simonswart91@gmail.com>
2026-09-18 15:23:51 +0200

    commit 2

---
+++ src/main/java/com/mycompany/testprep1/IProduct.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testprep1;
+
+/**
+ *
+ * @author simon
+ */
+public interface IProduct {
+    int TotalSales(int[][] productSales);
+    double AverageSales(int [][] productSales);
+    int MaxSale(int[][] productSales);
+    int MinSale(int[][] productSales);
+}
