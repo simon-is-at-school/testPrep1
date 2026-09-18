853e64c5b159d3f432dd3d4bb8a1aacbce431dd4
simon-is-a-dev <simonswart91@gmail.com>
2026-09-18 15:19:20 +0200

    commit 1

---
+++ src/main/java/com/mycompany/testprep1/TestPrep1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testprep1;
+
+/**
+ *
+ * @author simon
+ */
+public class TestPrep1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
