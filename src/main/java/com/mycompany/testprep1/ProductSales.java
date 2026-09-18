50228a7511a62b09ae9c069aa647c36283dfe1cb
simon-is-a-dev <simonswart91@gmail.com>
2026-09-18 15:44:08 +0200

    Welp that was something

--- src/main/java/com/mycompany/testprep1/ProductSales.java
+++ src/main/java/com/mycompany/testprep1/ProductSales.java
@@ -8,6 +8,69 @@ package com.mycompany.testprep1;
  *
  * @author simon
  */
-public class ProductSales {
+public class ProductSales implements IProduct {
+
+    @Override
+    public int TotalSales(int[][] productSales) {
+        int total = 0;
+
+        for (int i = 0; i < productSales.length; i++) {
+            for (int j = 0; j < productSales[i].length; j++) {
+                total += productSales[i][j];
+            }
+        }
+
+        return total;
+    }
+
+    @Override
+    public double AverageSales(int[][] productSales) {
+        int total = TotalSales(productSales);
+        int count = productSales.length * productSales[0].length;
+
+        return (double) total / count;
+    }
+
+    @Override
+    public int MaxSale(int[][] productSales) {
+        int max = productSales[0][0];
+        for (int i = 0; i < productSales.length; i++) {
+            for (int j = 0; j < productSales[i].length; j++) {
+                if (productSales[i][j] > max) {
+                    max = productSales[i][j];
+                }
+            }
+        }
+
+        return max;
+    }
+
+    @Override
+    public int MinSale(int[][] productSales) {
+        int min = productSales[0][0];
+        for (int i = 0; i < productSales.length; i++) {
+            for (int j = 0; j < productSales[i].length; j++) {
+                if (productSales[i][j] < min) {
+                    min = productSales[i][j];
+                }
+            }
+        }
+
+        return min;
+    }
     
+    public String display(int[][] productSales){
+      StringBuilder report = new StringBuilder();
+
+        report.append("PRODUCT SALES REPORT - 2025\n")
+              .append("------------------------------------------------\n")
+              .append("Total Sales: ").append(TotalSales(productSales)).append("\n")
+              .append("Average Sales: ").append(String.format("%.2f", AverageSales(productSales))).append("\n")
+              .append("Maximum Sale: ").append(MaxSale(productSales)).append("\n")
+              .append("Minimum Sale: ").append(MinSale(productSales)).append("\n")
+              .append("------------------------------------------------\n");
+
+        return report.toString();
+    }
+
 }
--- src/main/java/com/mycompany/testprep1/TestPrep1.java
+++ src/main/java/com/mycompany/testprep1/TestPrep1.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.testprep1;
 
 /**
@@ -11,6 +10,13 @@ package com.mycompany.testprep1;
 public class TestPrep1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[][] productSales = {
+            {300, 150, 700}, // Year 1: Q1, Q2, Q3
+            {250, 200, 600} // Year 2: Q1, Q2, Q3
+        };
+        
+        ProductSales ps = new ProductSales();
+        
+        System.out.println(ps.display(productSales));
     }
 }
